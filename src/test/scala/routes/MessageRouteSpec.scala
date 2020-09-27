42b6f9e145b3a60d3d4e74b031c217d7086c9e90
Ben Drawer <benjamin.drawer@tray.io>
2020-09-27 13:14:49 +0100

    More unit tests

--- src/test/scala/routes/MessageRouteSpec.scala
+++ src/test/scala/routes/MessageRouteSpec.scala
@@ -87,5 +87,106 @@ class MessageRouteSpec extends RouteSpec {
         )
       }
     }
+
+    "retrieve a list of messages from a server with custom limit parameter" in {
+      Get("/messages?server=1&limit=2") ~> addCredentials(testLogins("member")) ~!> routes ~> check {
+        status shouldEqual StatusCodes.OK
+        responseAs[List[ChildMessage]] shouldEqual List(
+          ChildMessage(
+            id = 1,
+            content = "Hello1",
+            sender = ChildUser(
+              id = 1,
+              username = "admin",
+              status = Status.withName("OFFLINE")
+            ),
+            createdAt = Timestamp.valueOf("2020-09-27 11:28:00")
+          ),
+          ChildMessage(
+            id = 2,
+            content = "Hello2",
+            sender = ChildUser(
+              id = 2,
+              username = "moderator",
+              status = Status.withName("OFFLINE")
+            ),
+            createdAt = Timestamp.valueOf("2020-09-27 11:28:00")
+          )
+        )
+      }
+    }
+
+    "retrieve a list of messages from a server with custom offset parameter" in {
+      Get("/messages?server=1&offset=1") ~> addCredentials(testLogins("member")) ~!> routes ~> check {
+        status shouldEqual StatusCodes.OK
+        responseAs[List[ChildMessage]] shouldEqual List(
+          ChildMessage(
+            id = 2,
+            content = "Hello2",
+            sender = ChildUser(
+              id = 2,
+              username = "moderator",
+              status = Status.withName("OFFLINE")
+            ),
+            createdAt = Timestamp.valueOf("2020-09-27 11:28:00")
+          ),
+          ChildMessage(
+            id = 3,
+            content = "Hello3",
+            sender = ChildUser(
+              id = 3,
+              username = "member",
+              status = Status.withName("OFFLINE")
+            ),
+            createdAt = Timestamp.valueOf("2020-09-27 11:28:00")
+          )
+        )
+      }
+    }
+
+    "retrieve a list of messages from a server with custom limit and offset parameter" in {
+      Get("/messages?server=1&limit=1&offset=1") ~> addCredentials(testLogins("member")) ~!> routes ~> check {
+        status shouldEqual StatusCodes.OK
+        responseAs[List[ChildMessage]] shouldEqual List(
+          ChildMessage(
+            id = 2,
+            content = "Hello2",
+            sender = ChildUser(
+              id = 2,
+              username = "moderator",
+              status = Status.withName("OFFLINE")
+            ),
+            createdAt = Timestamp.valueOf("2020-09-27 11:28:00")
+          )
+        )
+      }
+    }
+
+    "throw error if limit exceeds 1000" in {
+      Get("/messages?server=1&limit=1001") ~> addCredentials(testLogins("member")) ~!> routes ~> check {
+        status shouldEqual StatusCodes.BadRequest
+        responseAs[Result[NoRootElement]] shouldEqual Failure(
+          "The limit must have a value between 0 and 1000, or be left blank."
+        )
+      }
+    }
+
+    "throw error if limit is less than 0" in {
+      Get("/messages?server=1&limit=-1") ~> addCredentials(testLogins("member")) ~!> routes ~> check {
+        status shouldEqual StatusCodes.BadRequest
+        responseAs[Result[NoRootElement]] shouldEqual Failure(
+          "The limit must have a value between 0 and 1000, or be left blank."
+        )
+      }
+    }
+
+    "throw error if offset is less than 0" in {
+      Get("/messages?server=1&offset=-1") ~> addCredentials(testLogins("member")) ~!> routes ~> check {
+        status shouldEqual StatusCodes.BadRequest
+        responseAs[Result[NoRootElement]] shouldEqual Failure(
+          "The offset must be at least 0, or left blank."
+        )
+      }
+    }
   }
 }
